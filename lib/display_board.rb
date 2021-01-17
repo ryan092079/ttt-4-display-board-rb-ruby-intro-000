e856a4df21f0d473c5a8df29de02a3520a2cac25
Ryan Waggoner <ryan@waggoner.co>
2021-01-17 22:35:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,7 +11,7 @@ def display_board
 end
 
 def display_board(board)
-  puts "#{board[0]}, #{board[1]}, #{board[2]}"
-  puts "#{board[3]}, #{board[4]}, #{board[5]}"
-  puts "#{board[6]}, #{board[7]}, #{board[8]}"
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
